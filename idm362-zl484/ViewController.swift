167fbf822609211edf04ebcb60c59795ca9045b5
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-01-19 10:39:38 -0500

    Initial Commit

---
+++ idm362-zl484/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idm362-zl484
+//
+//  Created by Zhengtian Li on 1/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
